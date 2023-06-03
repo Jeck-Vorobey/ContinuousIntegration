52ab8b4431a3018afe651a276c5aeda3f31b0d46
Evgeni <ewgeny11@mail.ru>
2023-06-03 21:06:47 +0300

    correction pom

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -23,7 +23,7 @@ public class StatisticsServiceTest {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 89, 6, 11, 11, 12};
-        long expected = 12;
+        long expected = 89;
 
         long actual = service.findMax(incomesInBillions);
 
